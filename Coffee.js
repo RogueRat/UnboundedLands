b021871d25a3fec3de1fc1c2bfed3e4553438d0d
RogueRat <birklaus@gmail.com>
2024-08-27 07:14:02 +0100

    Limits to picture resizing & more

--- Coffee.js
+++ Coffee.js
@@ -3,6 +3,7 @@
 function reduceSize(element) {
         let oldSize = element.offsetWidth;
         let newSize = oldSize - 50;
+        if(newSize < 50) return;
         let command = "width:" + newSize + "px";
         /*window.alert("Old: " + oldSize + " and new: " + newSize + " and command: " + command);*/
         element.style = command;
@@ -11,6 +12,7 @@ function reduceSize(element) {
 function increaseSize(element) {
     let oldSize = element.offsetWidth;
     let newSize = oldSize + 50;
+    if(newSize > 1000) return;
     let command = "width:" + newSize + "px";
     element.style = command;
 }
